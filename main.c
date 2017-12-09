b087bc75642ab5ec9bef93683482732fc6b03b9a
Caio Toledo <caio_toledo_2@hotmail.com>
2017-12-09 18:17:42 -0200

    Printf mais profissional

--- main.c
+++ main.c
@@ -9,10 +9,8 @@ int main ()
 	printf("Hello World\n");
 	for (int i = 0; i < 10; i++)
 	{
-		printf("OPAAA %d!\n", i+1);
+		printf("Contador: %d!\n", i+1);
 	}
-	printf("Mais uma bobagem sem motivos!!!\n");
-	printf("Testando Alias!!\n");
 	printf("Author: Caio Vinicius de Toledo\n");
 	printf("Version %s\n", VERSION);
 }
