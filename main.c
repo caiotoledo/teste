89d69a669382a45663d862655cdbac76495a7959
Caio Toledo <caio_toledo_2@hotmail.com>
2017-12-09 17:54:53 -0200

    Implementado Contador de OPA

--- main.c
+++ main.c
@@ -4,7 +4,10 @@
 int main ()
 {
 	printf("Hello World\n");
-	printf("OPAAA!\n");
+	for (int i = 0; i < 10; i++)
+	{
+		printf("OPAAA %d!\n", i);
+	}
 	printf("Mais uma bobagem sem motivos!!!\n");
 	printf("Testando Alias!!n");
 }
