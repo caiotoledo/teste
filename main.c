a32853e673ee2ce95182701b65a5876d3c783cd3
Caio Toledo <caio_toledo_2@hotmail.com>
2017-12-09 17:52:54 -0200

    Adicionando meu Nome e melhorando printf

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 /* Inicio do Codigo Principal */
 int main ()
 {
+
 	printf("Hello World\n");
 	for (int i = 0; i < 10; i++)
 	{
 		printf("OPAAA %d!\n", i);
 	}
 	printf("Mais uma bobagem sem motivos!!!\n");
-	printf("Testando Alias!!n");
+	printf("Testando Alias!!\n");
+	printf("Caio Vinicius de Toledo\n");
 }
